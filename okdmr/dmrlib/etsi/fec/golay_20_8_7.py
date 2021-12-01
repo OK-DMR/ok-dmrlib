9d5edf37297442f8a0eeee41352c2cf51fccb062
Marek Sebera <marek.sebera@gmail.com>
2021-12-01 21:08:27 +0100

    golay[20,8,7] docs and fec tests directory

--- okdmr/dmrlib/etsi/fec/golay_20_8_7.py
+++ okdmr/dmrlib/etsi/fec/golay_20_8_7.py
@@ -9,7 +9,7 @@ from okdmr.dmrlib.etsi.fec.fec_utils import (
 
 class Golay2087:
     """
-    ETSI TS 102 361-1 V2.5.1 (2017-10) - B.3.1 Golay (20,8)
+    ETSI TS 102 361-1 V2.5.1 (2017-10) - B.3.1 Golay (20,8,7)
     """
 
     GENERATOR_MATRIX: numpy.ndarray = numpy.array(
@@ -33,6 +33,11 @@ class Golay2087:
 
     @staticmethod
     def check(bits: bitarray) -> bool:
+        """
+        Verifies Golay FEC in 12 end bits of input bitarray
+        :param bits:
+        :return: check result
+        """
         assert len(bits) == 20, "Golay20,8,7 expects exactly 20 bits"
         return numpy.array_equal(
             get_syndrome_for_word(
@@ -43,6 +48,11 @@ class Golay2087:
 
     @staticmethod
     def generate(bits: bitarray) -> bitarray:
+        """
+        Returns 8 bits (input) with 12 bits of Golay FEC
+        :param bits:
+        :return: 20 bits (8 data bits + 12 FEC bits)
+        """
         assert (
             len(bits) == 8
         ), "Golay20,8,7 expects 8 bits of data to add 12 bits of parity"
