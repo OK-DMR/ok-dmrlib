c39af53a698b7c30092d1e10b70867874b862b6c
Marek Sebera <marek.sebera@gmail.com>
2021-12-22 11:38:32 +0100

    saving work

--- okdmr/dmrlib/etsi/crc/crc9.py
+++ okdmr/dmrlib/etsi/crc/crc9.py
@@ -1,22 +1,17 @@
 from bitarray import bitarray
 from bitarray.util import int2ba
-from crc import CrcCalculator, Configuration
+from crccheck.crc import CrcBase
 
 from okdmr.dmrlib.utils.bits_bytes import bytes_to_bits
 
 
-class CRC9:
-    CALC: CrcCalculator = CrcCalculator(
-        configuration=Configuration(
-            width=9,
-            polynomial=0x59,  # without msb bit
-            init_value=0x00,
-            reverse_input=False,
-            reverse_output=True,
-            final_xor_value=0x00,
-        ),
-        table_based=True,
-    )
+class CRC9(CrcBase):
+    _width = 9
+    _poly = 0x59
+    _reflect_input = False
+    _reflect_output = False
+    _xor_output = 0x00
+    _names = ("CRC-9",)
 
     @staticmethod
     def check(
@@ -26,68 +21,27 @@ class CRC9:
         crc32: bytes = None,
         mask: int = 0x00,
     ) -> bool:
-        source_data: bitarray = bytes_to_bits(data, endian="little")
+        source_data: bitarray = bytes_to_bits(data, endian="big")
         if crc32 is not None:
             assert len(crc32) == 4, "32-bit CRC must be exactly 4-bytes long"
             source_data += bytes_to_bits(crc32, endian="big")
         else:
             dbsnba = int2ba(serial_number, length=7, endian="little", signed=False)
-            # dbsnba.reverse()
             source_data += dbsnba
 
-        # source_data.reverse()
+        print(
+            "crccheck",
+            (
+                CRC9.calc(data=source_data),
+                CRC9.calc(data=source_data.tobytes()),
+                CRC9.calc(data=source_data.tolist()),
+            ),
+            "expected",
+            (crc9, crc9 ^ 0x1FF, crc9 ^ mask),
+        )
 
-        for sde in ("little", "big"):
-            for polynom in (0x59,):
-                for rev_in in (True, False):
-                    for rev_out in (True, False):
-                        for xor_val in (0x00,):
-
-                            bfdata: bitarray = bytes_to_bits(data, endian=sde)
-                            bfdata += int2ba(serial_number, length=7, endian=sde)
-
-                            _calc = CrcCalculator(
-                                configuration=Configuration(
-                                    width=9,
-                                    polynomial=polynom,
-                                    init_value=0x00,
-                                    reverse_input=rev_in,
-                                    reverse_output=rev_out,
-                                    final_xor_value=xor_val,
-                                ),
-                                table_based=True,
-                            )
-
-                            if _calc.verify_checksum(
-                                data=bfdata.tobytes(), expected_checksum=crc9 ^ mask
-                            ):
-                                print(
-                                    polynom,
-                                    "rev_in",
-                                    rev_in,
-                                    "rev_out",
-                                    rev_out,
-                                    xor_val,
-                                )
-                                print("FOUND A sn %d" % serial_number)
-                                # return True
-
-                            if _calc.verify_checksum(
-                                data=bfdata.tobytes(), expected_checksum=crc9
-                            ):
-                                print(
-                                    polynom,
-                                    "rev_in",
-                                    rev_in,
-                                    "rev_out",
-                                    rev_out,
-                                    xor_val,
-                                )
-                                print("FOUND B sn %d" % serial_number)
-                                # return True
-
-        return _calc.verify_checksum(data=source_data, expected_checksum=crc9)
+        return False
 
     @staticmethod
     def calculate(data: bitarray) -> int:
-        return CRC9.CALC.calculate_checksum(data=data.tobytes())
+        return 0
--- okdmr/tests/dmrlib/etsi/crc/test_crc9.py
+++ okdmr/tests/dmrlib/etsi/crc/test_crc9.py
@@ -5,147 +5,40 @@ from okdmr.dmrlib.etsi.crc.crc_masks import CrcMasks
 
 
 def test_crc9_non_last_block():
+    # fmt:off
     # data, serial number, expected crc9, appropriate mask
     data: List[Tuple[str, int, int, CrcMasks, Union[None, str]]] = [
         # last 3/4 block confirmed
         # ("000000010136410000000000a5ac0080", 0, 476, CrcMasks.Rate34DataContinuation, "a5ac0080")
         # 3/4 blocks confirmed (single continuous transmission from hytera ms)
-        (
-            "47004d00500054002e004a0047004100",
-            17,
-            459,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "2e00570047005400500044002e004a00",
-            16,
-            138,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "4a0047005400470050004d0047004a00",
-            15,
-            46,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "41004a00470044002e004a0041002e00",
-            14,
-            63,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "4a002e00540047004d00500057005400",
-            13,
-            427,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "4a002e004d0041005700470044005000",
-            12,
-            174,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "41004700540050005700470044005400",
-            11,
-            60,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "2e004a004700570054004d0044002e00",
-            10,
-            193,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "4a00570041004d004700440050005400",
-            9,
-            171,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "47004a002e00410047004a0050005400",
-            8,
-            199,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "540044004a00570041004d0050005400",
-            7,
-            251,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "50004a002e00540047004a0050004100",
-            6,
-            448,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "54004400410057004a004d0047005400",
-            5,
-            363,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "470041002e00540047004a0050004d00",
-            4,
-            198,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "470041002e005400470054002e004a00",
-            3,
-            375,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "4100470054002e00410047002e005400",
-            2,
-            2,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "4a00470044002e00540047004a002e00",
-            1,
-            65,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
-        (
-            "0100000101004d004d00470054002e00",
-            0,
-            409,
-            CrcMasks.Rate34DataContinuation,
-            None,
-        ),
+        ("47004d00500054002e004a0047004100", 17, 459, CrcMasks.Rate34DataContinuation, None,),
+        ("2e00570047005400500044002e004a00", 16, 138, CrcMasks.Rate34DataContinuation, None,),
+        ("4a0047005400470050004d0047004a00", 15, 46, CrcMasks.Rate34DataContinuation, None,),
+        ("41004a00470044002e004a0041002e00", 14, 63, CrcMasks.Rate34DataContinuation, None,),
+        ("4a002e00540047004d00500057005400", 13, 427, CrcMasks.Rate34DataContinuation, None,),
+        ("4a002e004d0041005700470044005000", 12, 174, CrcMasks.Rate34DataContinuation, None,),
+        ("41004700540050005700470044005400", 11, 60, CrcMasks.Rate34DataContinuation, None,),
+        ("2e004a004700570054004d0044002e00", 10, 193, CrcMasks.Rate34DataContinuation, None,),
+        ("4a00570041004d004700440050005400", 9, 171, CrcMasks.Rate34DataContinuation, None,),
+        ("47004a002e00410047004a0050005400", 8, 199, CrcMasks.Rate34DataContinuation, None,),
+        ("540044004a00570041004d0050005400", 7, 251, CrcMasks.Rate34DataContinuation, None,),
+        ("50004a002e00540047004a0050004100", 6, 448, CrcMasks.Rate34DataContinuation, None,),
+        ("54004400410057004a004d0047005400", 5, 363, CrcMasks.Rate34DataContinuation, None,),
+        ("470041002e00540047004a0050004d00", 4, 198, CrcMasks.Rate34DataContinuation, None,),
+        ("470041002e005400470054002e004a00", 3, 375, CrcMasks.Rate34DataContinuation, None,),
+        ("4100470054002e00410047002e005400", 2, 2, CrcMasks.Rate34DataContinuation, None,),
+        ("4a00470044002e00540047004a002e00", 1, 65, CrcMasks.Rate34DataContinuation, None,),
+        ("0100000101004d004d00470054002e00", 0, 409, CrcMasks.Rate34DataContinuation, None,),
     ]
+    # fmt:on
 
     for testtuple in data:
         print(
             "::",
-            "datalen %d" % len(bytes.fromhex(testtuple[0])),
             CRC9.check(
                 data=bytes.fromhex(testtuple[0]),
                 serial_number=testtuple[1],
-                crc9=testtuple[2] ^ 0x1FF,
+                crc9=testtuple[2],
                 mask=testtuple[3].value,
                 crc32=bytes.fromhex(testtuple[4]) if testtuple[4] else None,
             ),
